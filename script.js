050b1ad24dbc27362763f470e702ac2dbfca812f
Martin <80769574+MartinfKristensen@users.noreply.github.com>
2022-01-28 11:19:32 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function generateNum() {
+  var num1 = document.getElementById("num1").value;
+  var num2 = document.getElementById("num2").value;
+  var p = document.getElementById("randomP");
+
+  if (num1 == null || num1 == "" || num2 == null || num2 == "") {
+    p.style.color = "red";
+    p.innerHTML = "Error: Empty Field";
+  } else {
+    var randNum = Math.floor(Math.random() * (num2 - num1) + num1);
+    if (randNum < 0) {
+      p.style.color = "red";
+      p.innerHTML = "Error: Number One Is Bigger Than The Second";
+    } else {
+      p.style.color = "black";
+      p.innerHTML = randNum;
+    }
+  }
+}
