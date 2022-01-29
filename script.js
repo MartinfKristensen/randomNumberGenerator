0fef186b6c76ffeba49348dd7b8215e78b0ad111
Martin <80769574+MartinfKristensen@users.noreply.github.com>
2022-01-29 19:56:44 +0100

    Fixed range bug 2

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function generateNum() {
   var num1 = document.getElementById("num1").value;
   var num2 = document.getElementById("num2").value;
-  num1 = Math.ceil(num1);
-  num2 = Math.floor(num2);
   var p = document.getElementById("randomP");
 
   if (num1 == null || num1 == "" || num2 == null || num2 == "") {
     p.style.color = "red";
     p.innerHTML = "Error: Empty Field";
   } else {
+    num1 = Math.ceil(num1);
+    num2 = Math.floor(num2);
     var randNum = Math.floor(Math.random() * (num2 - num1) + num1);
     if (randNum < 0) {
       p.style.color = "red";
