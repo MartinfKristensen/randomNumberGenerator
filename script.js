68991f49852bb1611814cd1d9cd88ea2d4d25911
Martin <80769574+MartinfKristensen@users.noreply.github.com>
2022-01-30 21:09:19 +0100

    Fixes bugs entirly

--- script.js
+++ script.js
@@ -2,19 +2,24 @@ function generateNum() {
   var num1 = document.getElementById("num1").value;
   var num2 = document.getElementById("num2").value;
   var p = document.getElementById("randomP");
+  var errorP = document.getElementById("errorP");
 
   if (num1 == null || num1 == "" || num2 == null || num2 == "") {
-    p.style.color = "red";
-    p.innerHTML = "Error: Empty Field";
+    errorP.style.display = "block";
+    errorP.style.color = "red";
+    errorP.innerHTML = "Error: Empty Field";
   } else {
-    num1 = Math.ceil(num1);
-    num2 = Math.floor(num2);
-    var randNum = Math.floor(Math.random() * (num2 - num1) + num1);
-    if (randNum < 0) {
-      p.style.color = "red";
-      p.innerHTML = "Error: Number One Is Bigger Than The Second";
+    num1 = Math.floor(num1);
+    num2 = Math.ceil(num2);
+    var randNum = Math.floor(Math.random() * (num2 - num1 + 1) + num1);
+    // console.log(randNum);
+    if (num1 > num2) {
+      errorP.style.display = "block";
+      errorP.style.color = "red";
+      errorP.innerHTML = "Error: Num1 > Num2";
+      p.innerHTML = "Error";
     } else {
-      p.style.color = "black";
+      errorP.style.display = "none";
       p.innerHTML = randNum;
     }
   }
